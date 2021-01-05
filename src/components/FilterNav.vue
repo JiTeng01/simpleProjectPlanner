dbd95db2a4ff47e98bf0a8414c362a486a1fd9d5
JiTeng01 <polygon960121@gmail.com>
2021-01-06 01:46:43 +0800

    first commit

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="filter-nav">
+      <button @click="updateFilter('all')" :class="{active: currentStatus === 'all'}">View All</button>
+      <button @click="updateFilter('completed')" :class="{active: currentStatus === 'completed'}">Completed</button>
+      <button @click="updateFilter('pending')" :class="{active: currentStatus === 'pending'}">Pending</button>
+  </nav>
+</template>
+
+<script>
+export default {
+    props: ['currentStatus'],
+    methods: {
+        updateFilter(status) {
+            this.$emit('filterChange', status)
+        }
+    }
+}
+</script>
+
+<style>
+.filter-nav button {
+    background: none;
+    border: none;
+    color: #bbb;
+    outline: none;
+    font-size: 12px;
+    text-transform: uppercase;
+    margin-right: 10px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    cursor: pointer;
+}
+.filter-nav button.active {
+    color: #555;
+}
+</style>
